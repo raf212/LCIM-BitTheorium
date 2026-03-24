fb294366c77c0096eabe92e92712ba86801cc011
nirab123456 <rifatulislam210@gmail.com>
2026-03-24 14:12:03 -0400

    PackedCellBranchPlugin.hpp::Continue

--- core/headers/PackedCellBranchPlugin.hpp
+++ core/headers/PackedCellBranchPlugin.hpp
@@ -9,7 +9,8 @@ class PackedCellBranchPlugin final
 {
 public:
     static constexpr size_t METACELL_COUNT = 64;
-    static constexpr uint32_t BRANCH_MAGIC = 0x41504342u;
+    static constexpr uint32_t BRANCH_MAGIC = 0x41504342u;//big-endian
+    static constexpr uint32_t EOF_HEADER = 0x72616600;//big-endian
     static constexpr uint32_t BRANCH_VERSION = 1u;
     static constexpr uint32_t BRANCH_SENTINAL = UINT32_MAX;
 
@@ -23,6 +24,7 @@ public:
 
     enum class MetaIndexOfAPCBranch : size_t 
     {
+        //identity
         MAGIC_ID = 0,
         VERSION = 1,
         CAPACITY = 2,
@@ -30,24 +32,42 @@ public:
         PARENT_BRANCH_ID = 4,
         LEFT_CHILD_ID = 5,
         RIGHT_CHILD_ID = 6,
-        CURRENT_ACTIVE_THREADS = 7,
+        CURRENT_TREE_POSITION = 7,
+        // runtime control
         BRANCH_DEPTH = 8,
         BRANCH_PRIORITY = 9,
-        CURRENT_TREE_POSITION = 10,
-        FLAGS = 11,
-        PAYLOAD_BEGIN = 12,
-        PAYLOAD_END = 13,
-        OCCUPANCY_SNAPSHOT = 14,
-        LOCAL_CLOCK48 = 15,
-        LAST_SPLIT_EPOCH = 16,
-        REGION_SIZE = 17,
-        REGION_COUNT = 18,
-        READY_REL_MASK = 19,
-        SPLIT_THRESHOLD_PERCENTAGE = 20,
-        MAX_DEPTH = 21,
-        RESERVED_UPTO = 63
-    };
+        FLAGS = 10,
+        CURRENT_ACTIVE_THREADS = 11,
+        OCCUPANCY_SNAPSHOT = 12,
+        SAFE_POINT = 13,
+        SPLIT_THRESHOLD_PERCENTAGE = 14,
+        MAX_DEPTH = 15,
+        //payload bounds
+        PAYLOAD_BEGIN = 16,
+        PAYLOAD_END = 17,
+        //timing
+        LOCAL_CLOCK48 = 18,
+        LAST_SPLIT_EPOCH = 19,
+        //region summery
+        REGION_SIZE = 20,
+        REGION_COUNT = 21,
+        READY_REL_MASK = 22,
 
+        //free exception 
+        RESERVED_23 = 23,
+        EOF_APC_HEADER = 63
+    };
+private:
+    std::atomic<packed64_t>* PackedCellContainerPtr_{nullptr};
+    size_t BranchCapacity_{0};
+    MasterClockConf* MasterClockConfPtr_{nullptr};
+
+    inline bool ValidMeteIdx_(MetaIndexOfAPCBranch idx) const noexcept
+    {
+        return PackedCellContainerPtr_ && static_cast<size_t>(idx) < BranchCapacity_ && static_cast<size_t>(idx) < METACELL_COUNT;
+    }
+public:
+    //checked top
     inline packed64_t PackValue32InPackedCellwithClock16(
         val32_t value32,
         tag8_t priority,
@@ -100,7 +120,8 @@ public:
     void WriteBrenchMeta32(
         MetaIndexOfAPCBranch idx,
         uint32_t value32,
-        tag8_t priority = ZERO_PRIORITY
+        tag8_t priority = ZERO_PRIORITY,
+        tag8_t rel_mask4 = REL_MASK4_NONE
     ) noexcept
     {
         size_t index = static_cast<size_t>(idx);
@@ -108,7 +129,7 @@ public:
         {
             return;
         }
-        PackedCellContainerPtr_[index].store(PackValue32InPackedCellwithClock16(value32, priority, PackedCellLocalityTypes::ST_PUBLISHED, REL_NONE), MoStoreSeq_);
+        PackedCellContainerPtr_[index].store(PackValue32InPackedCellwithClock16(value32, priority, PackedCellLocalityTypes::ST_PUBLISHED, rel_mask4), MoStoreSeq_);
         PackedCellContainerPtr_[index].notify_all();
     }
 
@@ -120,17 +141,30 @@ public:
         PackedCellContainerPtr_[idx].notify_all();
     }
 
-private:
-    std::atomic<packed64_t>* PackedCellContainerPtr_{nullptr};
-    size_t BranchCapacity_{0};
-    MasterClockConf* MasterClockConfPtr_{nullptr};
-
-    inline bool ValidMeteIdx_(MetaIndexOfAPCBranch idx) const noexcept
+    inline bool CASMeta32(
+        MetaIndexOfAPCBranch idx,
+        uint32_t expected_value,
+        uint32_t desired_value,
+        tag8_t priority = ZERO_PRIORITY,
+        tag8_t rel_mask = REL_MASK4_NONE
+    ) noexcept
     {
-        return PackedCellContainerPtr_ && static_cast<size_t>(idx) < BranchCapacity_ && static_cast<size_t>(idx) < METACELL_COUNT;
+        if (!ValidMeteIdx_(idx))
+        {
+            return false;
+        }
+        const size_t index = static_cast<size_t>(idx);
+        packed64_t expected_packed = PackedCellContainerPtr_[index].load(MoLoad_);
+        if (PackedCell64_t::ExtractValue32(expected_packed) != expected_value)
+        {
+            return false;
+        }
+
+        packed64_t desired_packed = PackValue32InPackedCellwithClock16(desired_value, priority, PackedCellLocalityTypes::ST_PUBLISHED, rel_mask, RelOffsetMode32::RELOFFSET_GENERIC_VALUE, PackedCellDataType::UnsignedPCellDataType);
+        
+        return PackedCellContainerPtr_[index].compare_exchange_strong(expected_packed, desired_packed, EXsuccess_, EXfailure_);
     }
 
-public:
     PackedCellBranchPlugin() noexcept = default;
 
     void Bind(std::atomic<packed64_t>* packed_cells, size_t capacity, MasterClockConf* master_clock_ptr) noexcept
@@ -182,21 +216,23 @@ public:
         WriteBrenchMeta32(MetaIndexOfAPCBranch::PARENT_BRANCH_ID, static_cast<uint32_t>(std::min<uint32_t>(parent_bramch_id, UINT32_MAX)), priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::LEFT_CHILD_ID, BRANCH_SENTINAL, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::RIGHT_CHILD_ID, BRANCH_SENTINAL, priority);
-        WriteBrenchMeta32(MetaIndexOfAPCBranch::CURRENT_ACTIVE_THREADS, NO_VAL, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::CURRENT_TREE_POSITION, static_cast<uint32_t>(current_tree_position), priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::BRANCH_DEPTH, current_depth, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::BRANCH_PRIORITY, branch_priority, priority);
-        WriteBrenchMeta32(MetaIndexOfAPCBranch::CURRENT_TREE_POSITION, static_cast<uint32_t>(current_tree_position), priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::FLAGS, NO_VAL, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::CURRENT_ACTIVE_THREADS, NO_VAL, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::OCCUPANCY_SNAPSHOT, NO_VAL, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::SAFE_POINT, NO_VAL, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::SPLIT_THRESHOLD_PERCENTAGE, split_threshold_percantage, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::MAX_DEPTH, max_depth, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::PAYLOAD_BEGIN, static_cast<uint32_t>(METACELL_COUNT), priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::PAYLOAD_END, static_cast<uint32_t>(std::min<size_t>(total_capacity, UINT32_MAX)), priority);
-        WriteBrenchMeta32(MetaIndexOfAPCBranch::OCCUPANCY_SNAPSHOT, NO_VAL, priority);
         WriteOrUpdateMetaClock48(priority, NO_VAL);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::LAST_SPLIT_EPOCH, NO_VAL, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::REGION_SIZE, region_size, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::REGION_COUNT, region_count, priority);
         WriteBrenchMeta32(MetaIndexOfAPCBranch::READY_REL_MASK, NO_VAL, priority);
-        WriteBrenchMeta32(MetaIndexOfAPCBranch::SPLIT_THRESHOLD_PERCENTAGE, split_threshold_percantage, priority);
-        WriteBrenchMeta32(MetaIndexOfAPCBranch::MAX_DEPTH, max_depth, priority);
+        WriteBrenchMeta32(MetaIndexOfAPCBranch::EOF_APC_HEADER, EOF_HEADER, priority);
 
     }
 
@@ -219,6 +255,25 @@ public:
         MasterClockConfPtr_->TouchAtomicPackedCellClockForCurrentThread(
             PackedCellContainerPtr_[static_cast<size_t>(MetaIndexOfAPCBranch::LOCAL_CLOCK48)], updated_full_clock_cell_easy_return_ptr
         );
+    }
+
+    bool TryAttachChildAPC(TreePosition side, uint32_t child_id) noexcept
+    {
+        MetaIndexOfAPCBranch current_tree_position = MetaIndexOfAPCBranch::EOF_APC_HEADER;
+        if (side == TreePosition::LEFT)
+        {
+            current_tree_position = MetaIndexOfAPCBranch::LEFT_CHILD_ID;
+        }
+        else if (side == TreePosition::RIGHT)
+        {
+            current_tree_position = MetaIndexOfAPCBranch::RIGHT_CHILD_ID;
+        }
+        else
+        {
+            return false;
+        }
+        
+        return CASMeta32(current_tree_position, BRANCH_SENTINAL, child_id);
     }
 
 };

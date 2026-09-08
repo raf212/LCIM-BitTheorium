34281ef149fba80cd4eef0550e3012a57184e747
nirab123456 <rifatulislam210@gmail.com>
2026-09-08 16:17:01 -0400

    CONTINUE::GHGFModelOfAPC

--- core/headers/Models/GHGFModelOfAPC.hpp
+++ core/headers/Models/GHGFModelOfAPC.hpp
@@ -6,11 +6,34 @@ namespace BidirectionalInMemGraph
 
     class GHGFModelConstructor : private VagueTemoraryPremativeFabric
     {
+    public:
+        using FCSpan = std::span<const float>;
     private:
         using GM = GHGFLayerModel;
         GM::GHGFCache Cache_{};
 
+        size_t GHGFSlotBegin_(uint32_t slot) const noexcept;
+        GM::GHGFNodeRole GHGFRole_(uint32_t slot) const noexcept;
+        bool IsExternalGHGFBuffer_(const float* data, size_t count) const noexcept;
+        bool IsLiveGHGFSlot_(uint32_t slot) noexcept;
+        bool IsGHGFPlanCurrent_() noexcept;
+        bool PredictGHGFNode_(uint32_t slot, uint32_t batch) noexcept;
+        bool UpdateGHGFNode_(uint32_t slot, uint32_t batch) noexcept;
+        bool PropogateGHGFErroe_(uint32_t child, uint32_t batch) noexcept;
+        bool PredictGHGFBatch(uint32_t batch) noexcept;
+        bool UpdateGHGFBatch(FCSpan observation, uint32_t batch) noexcept;
+        bool CopyGHGFPrediction_(FCSpan prediction, uint32_t batch) noexcept;
+
+        float* GHGFRegion(uint32_t slot, uint32_t cell_offset) noexcept;
+        float* GHGFStateRow_(uint32_t slot, GM::GHGFStateRow row) noexcept;
+        float* GHGFErrorRow_(uint32_t slot, GM::GHGFErrorRow row) noexcept;
+
+        void InvalidateGHGFModel_() noexcept;
+        void ResetGHGFModel_() noexcept;
+
+        uint64_t GHGFParentMask_(uint32_t slot, FabricSegments axis) noexcept;
     public :
+
 
         bool InitializeGHGFFabric(
             uint32_t slot_count,
@@ -28,10 +51,6 @@ namespace BidirectionalInMemGraph
             return HasDefaultRegionTable_ && CreateAPC(desired_apc, DefaultRegionTable_);
         }
 
-        size_t GHGFSlotBegin_(uint32_t slot) const noexcept;
-        bool IsLiveGHGFSlot_(uint32_t slot) noexcept;
-        bool IsGHGFPlanCurrent_() noexcept;
-        
         
             
     };
